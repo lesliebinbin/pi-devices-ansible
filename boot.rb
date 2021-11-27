0a6b2b45096b5d57190921338023ed1710b3662e
Leslie Binbin <lesliebinbin19900129@gmail.com>
2021-11-27 12:23:12 +1100

    initial play around

---
+++ boot.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+require 'yaml'
+def boot
+  mount_points, headless_configs = YAML.load_file('headless-pi-init.yml', symbolize_names: true)[:sdcard].values_at(
+    :mount_points, :headless_configs
+  )
+  mount_points.each do |m_point|
+    headless_configs.each do |h_config|
+      puts h_config
+      Dir["#{h_config}/*"].each do |filename|
+        FileUtils.cp(filename, "#{m_point}/boot")
+      end
+    end
+  end
+end
+
+boot if $PROGRAM_NAME == __FILE__
